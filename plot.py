9c7273525f1fe4795e09c0ca80588e35c8f30357
Andrew Knowles <ajknowles@optonline.net>
2019-07-24 10:01:16 -0400

    Better Comments

--- plot.py
+++ plot.py
@@ -11,6 +11,7 @@ plot = [[0, 0, 0, 0, 0],
         [0, 0, 2, 0, 0]]
 
 
+# scroll through list to find location of house
 def findHouse():
     for r in plot:
         for c in r:
@@ -18,8 +19,10 @@ def findHouse():
                 return[plot.index(r), r.index(c)]
 
 
+# sets drone starting position for first move
 prevPos = findHouse()
 
+# takeoff
 drone.connect()
 time.sleep(5)
 drone.takeoff()
@@ -27,20 +30,21 @@ time.sleep(5)
 drone.move_up(40)
 time.sleep(5)
 
+# scroll through list and find unwatered lawn
 for r in plot:
     for c in r:
         if c == 1:
             x = (plot.index(r) - prevPos[0]) * 40
             y = (r.index(c) - prevPos[1]) * 40
-            drone.go_xyz_speed(x, y, 0, 20)
+            drone.go_xyz_speed(x, y, 0, 20)  # move to unwatered lawn space
             time.sleep(5)
-            prevPos = [plot.index(r), r.index(c)]
-            r[r.index(c)] = 0
+            prevPos = [plot.index(r), r.index(c)]  # set starting pos for next move to current location
+            r[r.index(c)] = 0  # mark current space as empty to tell drone to move on
 
 time.sleep(3)
 x = (findHouse()[0] - prevPos[0]) * 40
 y = (findHouse()[1] - prevPos[1]) * 40
-drone.go_xyz_speed(x, y, 0, 20)
+drone.go_xyz_speed(x, y, 0, 20)  # moves back to initial position (house)
 time.sleep(3)
 drone.land()
 
